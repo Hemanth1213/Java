2a076751ed1f0003372a2f44ff6c22a6fc65dbbe
Hemanth1213 <atmurihemanth@gmail.com>
2025-06-17 14:02:02 +0530

    Create Operators.java

---
+++ Operators.java
@@ -0,0 +1,14 @@
+class Operators
+{
+public static void main(String args[])
+{
+int a=2,b=4;
+System.out.println("Arithmetic operators:");
+System.out.println("a+b="+(a+b));
+System.out.println("a-b="+(a-b));
+System.out.println("a*b="+(a*b));
+System.out.println("a/b="+(a/b));
+System.out.println("a%b="+(a%b));
+System.out.println(Add(a+b));
+}
+}
