f44dac1bc90afe9d3fb8ebeaa28731f8ac469b85
Hemanth1213 <atmurihemanth@gmail.com>
2025-06-17 14:05:03 +0530

    Create Logical.java

---
+++ Logical.java
@@ -0,0 +1,11 @@
+class  Logical
+{
+public static void main(String args[])
+{
+int a=10,b=12;
+boolean x=true,y=false;
+System.out.println("x&&y:"+(x&&y));
+System.out.println("x||y:"+(x||y));
+System.out.println("!x:"+(!x));
+}
+}
