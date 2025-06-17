f1c66c5dccca385af7271ca3e508295765d71a6b
Hemanth1213 <atmurihemanth@gmail.com>
2025-06-17 14:18:24 +0530

    Create Assignment.java

---
+++ Assignment.java
@@ -0,0 +1,21 @@
+class Assignment
+{
+public static void main(String args[])
+{
+int a=10;
+int b=5;
+System.out.println("intial values:");
+System.out.println("a="+a);
+System.out.println("b="+b);
+a+=b;
+System.out.println("After a+=b:a="+a);
+a-=b;
+System.out.println("After a-=b:a="+a);
+a*=b;
+System.out.println("After a*=b:a="+a);
+a/=b;
+System.out.println("After a/=b:a="+a);
+a%=b;
+System.out.println("After a%=b:a="+a);
+}
+}
